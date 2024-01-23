626ca722985d2e5ffd8801d290eed146fddf27ea
Sadman_Araf <saifursarker17@gmail.com>
2024-01-23 19:17:08 +0600

    Individual article page proto done

--- src/routes/[article]/+page.svelte
+++ src/routes/[article]/+page.svelte
@@ -1,1 +1,14 @@
-<h1>ARaf</h1>
+<script>
+	let email = '';
+	import { db } from '../../lib/firebase';
+	import { doc, onSnapshot } from 'firebase/firestore';
+	let docRef = doc(db, 'users', 'VaH2ErpbFJUkQDbAraXwyBfy7do1');
+	const unsubDoc = onSnapshot(docRef, (doc) => {
+		console.log(doc.data(), doc.id);
+		let data = doc.data();
+		email = data.email;
+		console.log(email);
+	});
+</script>
+
+<h1>{email}</h1>
